d7001ba3525934035b29ff433ab5e532c0e8c954
Henry Caldwell <henrycaldwell2005@gmail.com>
2026-05-08 21:55:33 -0400

    test(publish): add no op publisher tests

---
+++ src/test/java/info/henrycaldwell/aggregator/publish/NoOpPublisherTest.java
@@ -0,0 +1,65 @@
+package info.henrycaldwell.aggregator.publish;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import info.henrycaldwell.aggregator.core.MediaRef;
+import info.henrycaldwell.aggregator.core.PublishRef;
+import info.henrycaldwell.aggregator.error.SpecException;
+
+public class NoOpPublisherTest {
+
+  @Nested
+  class Constructor {
+
+    @Test
+    void acceptsMinimalConfig() {
+      Config config = ConfigFactory.parseString("""
+          name = publisher
+          type = no_op
+          """);
+
+      assertDoesNotThrow(() -> new NoOpPublisher(config));
+    }
+
+    @Test
+    void throwsOnUnknownKey() {
+      Config config = ConfigFactory.parseString("""
+          name = publisher
+          type = no_op
+          extra = value
+          """);
+
+      SpecException exception = assertThrows(SpecException.class, () -> new NoOpPublisher(config));
+
+      assertTrue(exception.getMessage().contains("Unknown configuration key"));
+      assertTrue(exception.getMessage().contains("key=extra"));
+    }
+  }
+
+  @Nested
+  class Publish {
+
+    @Test
+    void returnsPublishRefWithNullUri() {
+      MediaRef media = new MediaRef("clip-1", null, null, "Title", "Broadcaster", "en", null);
+      Config config = ConfigFactory.parseString("""
+          name = publisher
+          type = no_op
+          """);
+      NoOpPublisher publisher = new NoOpPublisher(config);
+
+      PublishRef result = publisher.publish(media);
+
+      assertNull(result.uri());
+    }
+  }
+}
